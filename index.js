28daa9cd7abba901ed46ddac94da8b001422559e
Kevin Kelley <kkelley622@gmail.com>
2022-04-21 16:06:24 -0500

    commit tree 1b7f0342943e37abdc9f27ee69f5edefc5c0ecad

--- index.js
+++ index.js
@@ -1,3 +1,26 @@
 class Formatter {
+
   //add static methods here
+  
+  static capitalize(word) {
+    return word[0].toUpperCase() + word.slice(1);
+  }
+
+  static sanitize(word) {
+    return word.replace(/[^A-Za-z0-9- ']+/g, "");
+  }
+
+  static titleize(sentence) {
+    const arr = ["the", "a", "an", "but", "of", "and", "for", "at", "by", "from"]
+
+    let modSentence = sentence.split(" ").map(s => {
+      if(!arr.includes(s)) {
+        return Formatter.capitalize(s)
+      }else {
+        return s
+      }
+    }).join(" ")
+    return Formatter.capitalize(modSentence);
+  }
+
 }
